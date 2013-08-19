fc75aa6d19bc6307e532c97e01527e121c0d2c60
Edrick Rudy Putra <chefmastervindex@gmail.com>
2013-08-19 21:17:23 +0800

    Title class implementation is done, but not yet defensive

--- Title.js
+++ Title.js
@@ -1,6 +1,52 @@
 /*
 A struct that will contain 3 components:
-String currentTitle
+String[] currentTitle
 bool isIgnored
-String formerTitle
+String[] originalTitle
+
+Init:
+(String[] originalTitleParameter, int shiftNumber)
+
+APIs
+String getFirstWord()
+String[] getCurrentTitle()
+String[] getOriginalTitle()
+void ignore()
+void include()
 */
+
+var Title = function(originalTitleParameter, shiftNumber) {
+	var self = this;
+	var originalTitle = originalTitleParameter;
+	var currentTitle = shiftOriginalTitle();
+	var isIgnored = false;
+	
+	this.getFirstWord = function() {
+		return currentTitle[0];
+	}
+	
+	this.getCurrentTitle = function() {
+		return currentTitle.slice(0);
+	}
+	
+	this.getOriginalTitle = function() {
+		return originalTitle.slice(0);
+	}
+	
+	this.ignore = () {
+		isIgnored = true;
+	}
+	
+	this.include = () {
+		isIgnored = false;
+	}
+	
+	function shiftOriginalTitle() {
+		var newTitle = new Array();
+		var i;
+		for (i=0; i<originalTitle.length; i++) {
+			newTitle[(i+shiftNumber)%originalTitle.length()] = originalTitle[i];
+		}
+		return newTitle;
+	}
+}
